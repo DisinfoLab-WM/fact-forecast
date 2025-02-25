4039b02a0941c282990df6cc01b8e7e4b50a8312
lillieayer <lillieayer@gmail.com>
2025-02-24 23:33:02 -0500

    added firebase config w dotenv to access firebase api key

---
+++ backend/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import 'dotenv/config';
+
+
+const firebaseConfig = {
+  apiKey:process.env.FIREBASE_API_KEY,
+  authDomain: "disinfopractice.firebaseapp.com",
+  databaseURL: "https://disinfopractice-default-rtdb.firebaseio.com",
+  projectId: "disinfopractice",
+  storageBucket: "disinfopractice.firebasestorage.app",
+  messagingSenderId: "834128285827",
+  appId: "1:834128285827:web:486cffa240652d86550bb6",
+  measurementId: "G-KNXV99Q533"
+};
+
+
+// Initialize Firebase  
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
