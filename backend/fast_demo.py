40c2804e66306d41d1823bc23cedf3d5c1ab9233
j-dewey <jcdewey@wm.edu>
2025-03-17 15:59:03 -0400

    FastAPI / Pyrebase interop started

---
+++ backend/fast_demo.py
@@ -0,0 +1,62 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+from os import environ as environment_variables
+
+import json
+import pyrebase
+from pyrebase.pyrebase import Database, PyreResponse
+from urllib3.contrib.appengine import HTTPError
+load_dotenv()
+
+'''
+    Features to add
+    1) Access specific article
+    2) Access most recent n articles
+    3) Push article to list
+'''
+
+API_KEY = environment_variables["FIREBASE_API_KEY"]
+
+API_CONFIG = {
+  "apiKey": API_KEY,
+  "authDomain": "disinfopractice.firebaseapp.com",
+  "databaseURL": "https://disinfopractice-default-rtdb.firebaseio.com",
+  "projectId": "disinfopractice",
+  "storageBucket": "disinfopractice.firebasestorage.app",
+  "messagingSenderId": "834128285827",
+  "appId": "1:834128285827:web:486cffa240652d86550bb6",
+  "measurementId": "G-KNXV99Q533"
+}
+
+app = FastAPI()
+firebase = pyrebase.initialize_app(API_CONFIG)
+db = firebase.database()
+
+def get_usa_articles(db: Database) -> Database:
+    return db.child("countries").child("USA").child("articles")
+
+@app.get("/usa/articles/{article_name}")
+async def get_specific_article(article_name: str):
+    global db
+    articles = get_usa_articles(db)
+    art = articles.child(article_name).get().val()
+    if art is None:
+        return {}
+    return art
+
+@app.get("/usa/recent/{n}")
+async def get_last_n_articles(n: int):
+    global db
+    articles = iter( get_usa_articles(db).get() )
+    recent_articles = {}
+    for i in range(n):
+        art: PyreResponse = next(articles)
+        recent_articles[str(i)] = art.val()
+    return recent_articles
+
+# TODO:
+#   Implement pushing to database
+@app.post("/usa/articles/{article_data}")
+async def post_article():
+    raise HTTPException(status_code=501, detail="pushing to database not yet implemented")
